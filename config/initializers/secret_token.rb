c8a66e165bb3a600db63d872c2ef78637214fa88
Sam Pohlenz <sam@sampohlenz.com>
2011-06-19 16:20:09 +0930

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SassImport::Application.config.secret_token = 'f32be9aec841d1aeaa4b720e04ad3e76e15a4694f19dac6e966e12559de96230dfe4d9e29d0cd93efec5257aab6e3dce2a890dd9dab29ada4df72f1dc03d4ace'
